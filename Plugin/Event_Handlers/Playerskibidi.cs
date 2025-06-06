40b5548a49805b8db1a06c01b67ae6dffd913ca5
Bartosz123pl <marine.daiwa@gmail.com>
2025-06-06 18:12:38 +0200

    Update Playerskibidi.cs

--- Plugin/Event_Handlers/Playerskibidi.cs
+++ Plugin/Event_Handlers/Playerskibidi.cs
@@ -12,48 +12,6 @@ namespace Plugin.Event_Handlers
             if (Plugin.Instance.CICASSIE == true)
             {
                 Plugin.Instance.CICASSIE = false;
-                foreach (var player in Player.List)
-                {
-                    if (player.Role.Team == Team.ChaosInsurgency)
-                    {
-                        player.IsBypassModeEnabled = false;
-
-                    }
-                }
-            }
-        }
-
-        public static void OnChangingRole(ChangingRoleEventArgs ev)
-        {
-
-            if (Plugin.Instance.CICASSIE == true && ev.Player.Role.Team != Team.ChaosInsurgency &&
-                (ev.Player.PreviousRole.Equals(RoleTypeId.ChaosRifleman) ||
-                ev.Player.PreviousRole.Equals(RoleTypeId.ChaosConscript) ||
-                ev.Player.PreviousRole.Equals(RoleTypeId.ChaosMarauder) ||
-                ev.Player.PreviousRole.Equals(RoleTypeId.ChaosRepressor)))
-            {
-                ev.Player.IsBypassModeEnabled = true;
-
-
-            }
-            if (Plugin.Instance.CICASSIE == true && ev.Player.Role.Team == Team.ChaosInsurgency &&
-               !(ev.Player.PreviousRole.Equals(RoleTypeId.ChaosRifleman) ||
-                ev.Player.PreviousRole.Equals(RoleTypeId.ChaosConscript) ||
-                ev.Player.PreviousRole.Equals(RoleTypeId.ChaosMarauder) ||
-                ev.Player.PreviousRole.Equals(RoleTypeId.ChaosRepressor)))
-            {
-                ev.Player.IsBypassModeEnabled = false;
-
-
-            }
-        }
-        public static void OnDeath(DiedEventArgs ev)
-        {
-            if (ev.Player.IsCHI == true && Plugin.Instance.CICASSIE == true)
-            {
-                ev.Player.IsBypassModeEnabled = false;
-
-
             }
         }
         public static void OnTesla(TriggeringTeslaEventArgs ev)
@@ -67,13 +25,5 @@ namespace Plugin.Event_Handlers
                 ev.IsTriggerable = false;
             }
         }
-        public static void OnCISpawned(SpawnedEventArgs ev)
-        {
-            if (ev.Player.Role.Team == Team.ChaosInsurgency && Plugin.Instance.CICASSIE == true)
-            {
-                ev.Player.IsBypassModeEnabled = true;
-            }
-        }
-
     }
 }
