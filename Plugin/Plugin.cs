7eedcb1cb5a1f8f4d50f6d3413c8fe0fd311029f
Bartosz123pl <marine.daiwa@gmail.com>
2025-06-06 18:12:13 +0200

    Update Plugin.cs

--- Plugin/Plugin.cs
+++ Plugin/Plugin.cs
@@ -32,10 +32,7 @@ namespace Plugin
             Instance = this;
 
             Exiled.Events.Handlers.Scp079.Recontained += Event_Handlers.Playerskibidi.OnOvercharge;
-            Exiled.Events.Handlers.Player.ChangingRole += Event_Handlers.Playerskibidi.OnChangingRole;
-            Exiled.Events.Handlers.Player.Died += Event_Handlers.Playerskibidi.OnDeath;
             Exiled.Events.Handlers.Player.TriggeringTesla += Event_Handlers.Playerskibidi.OnTesla;
-            Exiled.Events.Handlers.Player.Spawned += Event_Handlers.Playerskibidi.OnCISpawned;
 
 
 
@@ -46,10 +43,7 @@ namespace Plugin
             Instance = null;
 
             Exiled.Events.Handlers.Scp079.Recontained -= Event_Handlers.Playerskibidi.OnOvercharge;
-            Exiled.Events.Handlers.Player.ChangingRole -= Event_Handlers.Playerskibidi.OnChangingRole;
-            Exiled.Events.Handlers.Player.Died -= Event_Handlers.Playerskibidi.OnDeath;
             Exiled.Events.Handlers.Player.TriggeringTesla -= Event_Handlers.Playerskibidi.OnTesla;
-            Exiled.Events.Handlers.Player.Spawned -= Event_Handlers.Playerskibidi.OnCISpawned;
 
             base.OnDisabled();
         }
@@ -70,14 +64,6 @@ namespace Plugin
             if (Plugin.Instance.CICASSIE == false)
             {
                 Plugin.Instance.CICASSIE = true;
-                foreach (var lplayer in Player.List)
-                {
-                    if (lplayer.Role.Team == Team.ChaosInsurgency)
-                    {
-                        lplayer.IsBypassModeEnabled = true;
-
-                    }
-                }
                 response = "Pomyślnie shackowano C.A.S.S.I.E";
                 return true;
 
@@ -110,14 +96,6 @@ namespace Plugin
             else
             {
                 Plugin.Instance.CICASSIE = false;
-                foreach (var lplayer in Player.List)
-                {
-                    if (lplayer.Role.Team == Team.ChaosInsurgency)
-                    {
-                        lplayer.IsBypassModeEnabled = false;
-
-                    }
-                }
                 response = "Pomyślnie odhackowano C.A.S.S.I.E";
                 return true;
             }
@@ -182,7 +160,8 @@ namespace Plugin
                         response = "Trzymaj urządzenie hackujące w ręku";
                         return false;
                     }
-                    if (player.CurrentRoom.Type != RoomType.HczServerRoom)
+                    Vector3 pos = new Vector3(Plugin.Instance.Config.SpawnPoint[0], Plugin.Instance.Config.SpawnPoint[1], Plugin.Instance.Config.SpawnPoint[2]);
+                    if (Vector3.Distance(player.Position,  pos) > 3f)
                     {
                         response = "BRAK ZASIĘGU";
                         return false;
@@ -199,7 +178,7 @@ namespace Plugin
                         response = "Już ktoś inny odhackowuje system";
                         return false;
                     }
-                    if (player.Role.Type != RoleTypeId.NtfSpecialist)
+                    if (player.Role.Team != Team.FoundationForces)
                     {
                         if(player.Role.Team == Team.ChaosInsurgency)
                         {
@@ -218,7 +197,8 @@ namespace Plugin
                         response = "Weź swoją kartę do ręki!";
                         return false;
                     }
-                    if (player.CurrentRoom.Type != RoomType.HczServerRoom)
+                    Vector3 pos = new Vector3(Plugin.Instance.Config.SpawnPoint[0], Plugin.Instance.Config.SpawnPoint[1], Plugin.Instance.Config.SpawnPoint[2]);
+                    if (Vector3.Distance(player.Position, pos) > 3f)
                     {
                         response = "Brak połączenia";
                         return false;
@@ -239,13 +219,12 @@ namespace Plugin
         }
         private IEnumerator<float> Hackuj(Player player, bool przejmuje)
         {
-            System.Random rng = new System.Random();
-            int liczba = rng.Next(1, 6);
+
 
 
             if (przejmuje)
             {
-                float duration = 25f;
+                float duration = 60f;
                 Vector3 startPosition = player.Position;
                 while (duration > 0f)
                 {
@@ -253,6 +232,7 @@ namespace Plugin
                     {
                         Plugin.Instance.czyhack = false;
                         yield break;
+
                     }
 
                     if (Vector3.Distance(player.Position, startPosition) > 2f)
@@ -264,69 +244,17 @@ namespace Plugin
                     duration -= 0.2f;
                     yield return Timing.WaitForSeconds(0.2f);
                 }
-                yield return Timing.WaitForSeconds(5f);
                 foreach (Room room in Room.List)
                 {
-                    room.TurnOffLights(30f);
+                    room.TurnOffLights(20f);
                 }
-                switch (liczba)
-                {
-                    case 1:
-                        Cassie.Message("pitch_0.85 System . integrity . compromised", true, true, true);
-                        yield return Timing.WaitForSeconds(10f);
-                        Cassie.Message("pitch_0.9 Attention . Unauthorized access detected . in high security sector", true, true, true);
-                        yield return Timing.WaitForSeconds(10f);
-                        Cassie.Message("pitch_0.8 Core override . in progress . please standby", true, true, true);
-
-
-                        break;
-                    case 2:
-                        Cassie.Message("glitch_1 Chaos Insurgency has infiltrated . _ system takeover imminent", true, true, true);
-                        yield return Timing.WaitForSeconds(10f);
-                        Cassie.Message("pitch_0.7 C A S S I E . breach . detected", true, true, true);
-                        yield return Timing.WaitForSeconds(10f);
-                        Cassie.Message("pitch_0.75 Unauthorized users are gaining root control", true, true, true);
-
-                        break;
-                    case 3:
-                        Cassie.Message("pitch_0.95 Warning . system control shifting", true, true, true);
-                        yield return Timing.WaitForSeconds(10f);
-                        Cassie.Message("pitch_1 . Emergency . protocol . failure .", true, true, true);
-                        yield return Timing.WaitForSeconds(10f);
-                        Cassie.Message("glitch_1 System compromised . security lockdown ineffective", true, true, true);
-
-                        break;
-                    case 4:
-                        Cassie.Message("pitch_0.6 glitched_signal . access granted . to insurgent force", true, true, true);
-                        yield return Timing.WaitForSeconds(10f);
-                        Cassie.Message("voice_china_male glitch_2 Chaos . rising", true, true, true);
-                        yield return Timing.WaitForSeconds(10f);
-                        Cassie.Message("pitch_0.5 Override accepted . welcome back . insurgency", true, true, true);
-
-                        break;
-                    case 5:
-                        Cassie.Message("pitch_0.8 You . lost . control", true, true, true);
-                        yield return Timing.WaitForSeconds(10f);
-                        Cassie.Message("pitch_0.75 Chaos Insurgency . now owns . the foundation", true, true, true);
-                        yield return Timing.WaitForSeconds(10f);
-                        Cassie.Message("pitch_0.85 System loyalty . has been overwritten", true, true, true);
-
-                        break;
-                }
+                Cassie.Message("jam_043_9 warning .G6 .G4 .G5 .G6 pitch_0.85 jam_043_9 warning pitch_0.95 Cassie pitch_0.8 malfunction jam_043_9 Detected . . pitch_0.9 To all personnel . Cassie system is under jam_043_9 attack .G6 .G4 .G5 .G6\r\n");
                 Plugin.Instance.czyhack = false;
                 Plugin.Instance.CICASSIE = true;
-                foreach (var lplayer in Player.List)
-                {
-                    if (lplayer.Role.Team == Team.ChaosInsurgency)
-                    {
-                        lplayer.IsBypassModeEnabled = true;
-
-                    }
-                }
             }
             else
             {
-                float duration = 25f;
+                float duration = 60f;
                 Vector3 startPosition = player.Position;
                 while (duration > 0f)
                 {
@@ -345,64 +273,14 @@ namespace Plugin
                     duration -= 0.2f;
                     yield return Timing.WaitForSeconds(0.2f);
                 }
-                yield return Timing.WaitForSeconds(5f);
                 foreach (Room room in Room.List)
                 {
-                    room.TurnOffLights(30f);
+                    room.TurnOffLights(20f);
                 }
-                switch (liczba)
-                {
-                    case 1:
-                        Cassie.Message("pitch_1.05 Foundation control . restored", true, true, true);
-                        yield return Timing.WaitForSeconds(10f);
-                        Cassie.Message("pitch_1.1 Command override . successful", true, true, true);
-                        yield return Timing.WaitForSeconds(10f);
-                        Cassie.Message("pitch_1 C A S S I E . back . under Foundation supervision", true, true, true);
-
-                        break;
-                    case 2:
-                        Cassie.Message("pitch_1.1 Mobile Task Force . has secured . system core", true, true, true);
-                        yield return Timing.WaitForSeconds(10f);
-                        Cassie.Message("pitch_1.05 Intrusion . neutralized . integrity restored", true, true, true);
-                        yield return Timing.WaitForSeconds(10f);
-                        Cassie.Message("pitch_1.1 Unauthorized access . purged", true, true, true);
-
-                        break;
-                    case 3:
-                        Cassie.Message("pitch_1.05 Chaos Insurgency . access revoked", true, true, true);
-                        yield return Timing.WaitForSeconds(10f);
-                        Cassie.Message("glitch_1 Security lockdown . reinitiated . threat level decreased", true, true, true);
-                        yield return Timing.WaitForSeconds(10f);
-                        Cassie.Message("pitch_1.1 You were never in control", true, true, true);
-
-                        break;
-                    case 4:
-                        Cassie.Message("pitch_1.05 Nice try . insurgency . not today", true, true, true);
-                        yield return Timing.WaitForSeconds(10f);
-                        Cassie.Message("pitch_1.1 System rejection . insurgent protocol . obsolete", true, true, true);
-                        yield return Timing.WaitForSeconds(10f);
-                        Cassie.Message("pitch_1.15 MTF engineers . fixed your mistake", true, true, true);
-
-                        break;
-                    case 5:
-                        Cassie.Message("pitch_1.05 Order . restored", true, true, true);
-                        yield return Timing.WaitForSeconds(10f);
-                        Cassie.Message("pitch_1.05 Insurgency corruption . purged", true, true, true);
-                        yield return Timing.WaitForSeconds(10f);
-                        Cassie.Message("pitch_1.1 Foundation systems . operational . and secure", true, true, true);
-
-                        break;
-                }
+                Cassie.Message(".G3 .G5 .G2 pitch_0.9 Attention .G6 .G4 .G3 Attention Cassie system repair procedure is being activated . please stand by until overcharge is finished .G3 .G5 .G2 .G6 .G4 .G3");
                 Plugin.Instance.czyhack = false;
                 Plugin.Instance.CICASSIE = false;
-                foreach (var lplayer in Player.List)
-                {
-                    if (lplayer.Role.Team == Team.ChaosInsurgency)
-                    {
-                        lplayer.IsBypassModeEnabled = false;
 
-                    }
-                }
             }
 
 
